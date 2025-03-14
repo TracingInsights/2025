96d644b631ad18f1436796d1ae94e5762ea8624f
TracingInsights Archive <tracinginsights+1@gmail.com>
2025-03-14 07:03:23 +0000

    ..

--- telFP1.py
+++ telFP1.py
@@ -246,7 +246,7 @@ def laps_data(
     # Remove rows where LapTime is null
     driver_laps = driver_laps[driver_laps.LapTime.notnull()]
     driver_laps = driver_laps.copy()
-    driver_laps["LapTime"] = driver_laps["LapTime"].dt.total_seconds()
+    driver_laps.loc[:, "LapTime"] = driver_laps["LapTime"].dt.total_seconds()
     return {
         "time": driver_laps["LapTime"].tolist(),
         "lap": driver_laps["LapNumber"].tolist(),
@@ -330,7 +330,7 @@ def telemetry_data(year, event, session: str, driver, lap_number):
     driver_laps = laps.pick_drivers(driver)
     driver_laps = driver_laps.copy()
     driver_laps = driver_laps[driver_laps.LapTime.notnull()]
-    driver_laps["LapTime"] = driver_laps["LapTime"].dt.total_seconds()
+    driver_laps.loc[:, "LapTime"] = driver_laps["LapTime"].dt.total_seconds()
 
     # Get the telemetry for lap_number
     selected_lap = driver_laps[driver_laps.LapNumber == lap_number]
