abb5a6e1e37c1bf6918d405112ee8e2a901dcbaa
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-04-06 19:48:27 +0530

    Update telR.py

--- telR.py
+++ telR.py
@@ -50,7 +50,7 @@ class TelemetryExtractor:
     # "Pre-Season Testing",
     "Australian Grand Prix",
     # 'Chinese Grand Prix',
-    # 'Japanese Grand Prix',
+    'Japanese Grand Prix',
     # 'Bahrain Grand Prix',
     # 'Saudi Arabian Grand Prix',
     # 'Miami Grand Prix',
@@ -73,7 +73,7 @@ class TelemetryExtractor:
     # 'Qatar Grand Prix',
     # 'Abu Dhabi Grand Prix',
 ]
-        self.sessions = sessions or ["Race"]
+        self.sessions = sessions or ["Practice 1" , "Practice 2", "Practice 3", "Qualifying" ,"Race"]
 
     def get_session(
         self, event: Union[str, int], session: str, load_telemetry: bool = False
