fb12b9195e7a97c3a0b9c7e93351bd6de0ffeffa
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-10-29 12:47:31 +0530

    Refactor laptimesR.py to update event list

--- laptimesR.py
+++ laptimesR.py
@@ -47,7 +47,30 @@ class LaptimeExtractor:
         self.ergast_client = ErgastClient()
 
         self.events = events or [
-            'Australian Grand Prix',
+            # "Australian Grand Prix",
+            "Chinese Grand Prix",
+            "Japanese Grand Prix",
+            "Bahrain Grand Prix",
+            'Saudi Arabian Grand Prix',
+            "Miami Grand Prix",
+            # "Emilia Romagna Grand Prix",
+            # "Monaco Grand Prix",
+            # 'Spanish Grand Prix',
+            # "Canadian Grand Prix",
+            # "Austrian Grand Prix",
+            # "British Grand Prix",
+            # "Belgian Grand Prix",
+            # "Hungarian Grand Prix",
+            # "Dutch Grand Prix",
+            # 'Italian Grand Prix',
+            # 'Azerbaijan Grand Prix',
+            # 'Singapore Grand Prix',
+            # 'United States Grand Prix',
+            # 'Mexico City Grand Prix',
+            # 'São Paulo Grand Prix',
+            # 'Las Vegas Grand Prix',
+            # 'Qatar Grand Prix',
+            # 'Abu Dhabi Grand Prix',
         ]
         self.sessions = sessions or ["Race"]
 
@@ -402,4 +425,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
