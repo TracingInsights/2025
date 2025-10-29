cd2982c0be219760a8f0ca5d82a4d763c58a7c89
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-10-29 14:46:56 +0530

    ..

--- laptimesR.py
+++ laptimesR.py
@@ -48,25 +48,25 @@ class LaptimeExtractor:
 
         self.events = events or [
             # "Australian Grand Prix",
-            "Chinese Grand Prix",
-            "Japanese Grand Prix",
+            # "Chinese Grand Prix",
+            # "Japanese Grand Prix",
             # "Bahrain Grand Prix",
             # 'Saudi Arabian Grand Prix',
-            # "Miami Grand Prix",
-            # "Emilia Romagna Grand Prix",
-            # "Monaco Grand Prix",
-            # 'Spanish Grand Prix',
-            # "Canadian Grand Prix",
-            # "Austrian Grand Prix",
-            # "British Grand Prix",
-            # "Belgian Grand Prix",
-            # "Hungarian Grand Prix",
-            # "Dutch Grand Prix",
-            # 'Italian Grand Prix',
-            # 'Azerbaijan Grand Prix',
-            # 'Singapore Grand Prix',
-            # 'United States Grand Prix',
-            # 'Mexico City Grand Prix',
+            "Miami Grand Prix",
+            "Emilia Romagna Grand Prix",
+            "Monaco Grand Prix",
+            'Spanish Grand Prix',
+            "Canadian Grand Prix",
+            "Austrian Grand Prix",
+            "British Grand Prix",
+            "Belgian Grand Prix",
+            "Hungarian Grand Prix",
+            "Dutch Grand Prix",
+            'Italian Grand Prix',
+            'Azerbaijan Grand Prix',
+            'Singapore Grand Prix',
+            'United States Grand Prix',
+            'Mexico City Grand Prix',
             # 'São Paulo Grand Prix',
             # 'Las Vegas Grand Prix',
             # 'Qatar Grand Prix',
@@ -427,4 +427,5 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
