d235475b174a5a08886599d51f618284624dceeb
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-10-29 15:11:45 +0530

    ..

--- laptimesR.py
+++ laptimesR.py
@@ -55,8 +55,8 @@ class LaptimeExtractor:
             "Miami Grand Prix",
             "Emilia Romagna Grand Prix",
             "Monaco Grand Prix",
-            'Spanish Grand Prix',
-            "Canadian Grand Prix",
+            # 'Spanish Grand Prix',
+            # "Canadian Grand Prix",
             # "Austrian Grand Prix",
             # "British Grand Prix",
             # "Belgian Grand Prix",
@@ -427,6 +427,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
