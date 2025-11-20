56c7b84e25903058ba7c775a4169e1c085c9d867
Tracing Insights <tracing.insights@gmail.com>
2025-11-20 21:44:16 +0530

    LVGP

--- telR.py
+++ telR.py
@@ -79,8 +79,8 @@ class TelemetryExtractor:
             # 'Singapore Grand Prix',
             # 'United States Grand Prix',
             # 'Mexico City Grand Prix',
-            'São Paulo Grand Prix',
-            # 'Las Vegas Grand Prix',
+            # 'São Paulo Grand Prix',
+            'Las Vegas Grand Prix',
             # 'Qatar Grand Prix',
             # 'Abu Dhabi Grand Prix',
         ]
