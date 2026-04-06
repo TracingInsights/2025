d3aa3a2c86a6778dd3d933a9c1fd1dd53bd5b715
Tracing Insights <tracing.insights@gmail.com>
2026-04-06 22:11:02 +0530

    Update LapTimes.py

--- LapTimes.py
+++ LapTimes.py
@@ -43,7 +43,7 @@ TARGET_EVENT_NAMES_LIST = [
     # "Emilia Romagna Grand Prix",
     # "Monaco Grand Prix",
     # "Spanish Grand Prix",
-    "Canadian Grand Prix",
+    # "Canadian Grand Prix",
     # "Austrian Grand Prix",
     "British Grand Prix",
     # "Belgian Grand Prix",
@@ -73,12 +73,12 @@ AVAILABLE_SESSIONS = [
 ]
 # Select one or more sessions from AVAILABLE_SESSIONS.
 TARGET_SESSIONS = [
-    "Practice 1",
-    "Practice 2",
-    "Practice 3",
-    "Qualifying",
+    # "Practice 1",
+    # "Practice 2",
+    # "Practice 3",
+    # "Qualifying",
     "Sprint Qualifying",
-    "Sprint",
+    # "Sprint",
     # "Race",
 ]
 invalid_target_sessions = sorted(set(TARGET_SESSIONS) - set(AVAILABLE_SESSIONS))
